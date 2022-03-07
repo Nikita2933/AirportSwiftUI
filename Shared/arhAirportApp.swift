4f4bac77daf3f8d7aba5de1494066faa758d65fd
Nikita <nikita.ivanov@btc-s.ru>
2022-03-07 10:47:09 +0300

    Initial Commit

---
+++ Shared/arhAirportApp.swift
@@ -0,0 +1,17 @@
+//
+//  arhAirportApp.swift
+//  Shared
+//
+//  Created by Никита Иванов on 07.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct arhAirportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
